f6a9596f56d400e67956519a53a37b89733c9fe5
carpaiz <32883274+carpaiz@users.noreply.github.com>
2024-02-18 12:02:09 -0600

    primer commit

---
+++ Estudiante.cpp
@@ -0,0 +1,24 @@
+#include "Persona.cpp"
+#include <iostream>
+using namespace std;
+class Estudiante : Persona{
+	// atributo Estudiante = carnet 
+	private : string carnet;
+	// constructor
+	public : 
+	Estudiante (){
+	}
+	Estudiante(string nom,string ape,string dir,int tel,string fn,string ca) 
+	: Persona(nom,ape,dir,tel,fn){
+		carnet = ca;
+	}
+	// Metodo
+	void leer(){
+		cout<<carnet<<endl;
+		cout<<nombres<<endl;
+		cout<<apellidos<<endl;
+		cout<<direccion<<endl;
+		cout<<telefono<<endl;
+		cout<<fecha_nacimiento<<endl;
+	}
+};
