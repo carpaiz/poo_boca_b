1e5861c7904aaf96edf97f84b82fe2ed3ae70141
carpaiz <32883274+carpaiz@users.noreply.github.com>
2024-02-18 12:23:50 -0600

    se creo el metodo principal main

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "Estudiante.cpp"
+#include <iostream>
+using namespace std;
+
+main(){
+	 // instancia de un objeto
+	 Estudiante estudiante = Estudiante("Armando","Cardona","Guatemala",123,"2024-02-18","7990");
+	 estudiante.leer();
+}
